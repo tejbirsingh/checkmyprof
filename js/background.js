11368f33c0b131755e4956acb19eeb28451c7fcb
tejbirsingh <tejbirsodhan@gmail.com>
2015-03-28 00:25:44 -0700

    Can get the professor's elements by xpath

--- js/background.js
+++ js/background.js
@@ -1,3 +1,18 @@
 /**
  * Created by tejbirsingh on 15-03-27.
  */
+debugger;
+var rateDiv, target, professorName;
+
+target = $x('/html/body/div[2]/div[4]/table[3]/tbody/tr/td[2]/a');
+
+function $x(path) {
+  var result = document.evaluate(path, document, null, XPathResult.ANY_TYPE, null);
+  var xnodes = [];
+  var xres;
+  while(xres = result.iterateNext()){
+    xnodes.push(xres);
+  }
+
+  return xnodes;
+}
